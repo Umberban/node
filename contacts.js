61365a376dc7a08cb1a9789c9415709eb838839b
Umberban <romanovborysv@gmail.com>
2023-05-23 23:48:57 +0300

    finished

---
+++ contacts.js
@@ -0,0 +1,71 @@
+import { nanoid } from 'nanoid'
+import * as fs from 'fs/promises';
+import * as path from 'path'
+const contactsPath = path.join("db","contacts.json");
+/*
+ * Розкоментуйте і запиши значення
+ */
+
+// TODO: задокументувати кожну функцію
+export async function listContacts() {
+    try{ 
+        const data = await fs.readFile(contactsPath); 
+        const contacts = JSON.parse(data);
+        return contacts;
+       }catch (err) {
+           console.log('Something went wrong..');
+           console.log(err.message);
+         }
+     }
+  
+  export async function getContactById(contactId) {
+    try{ 
+     const data = await fs.readFile(contactsPath); 
+     const text = JSON.parse(data);
+     console.log('Contact you are searching for')
+     console.log(text.find(contact=> contact.id === contactId));
+        // return text.find(contact=> contact.id === contactId);
+    }catch (err) {
+        console.log('Something went wrong..');
+        console.log(err.message);
+      }
+  }
+  
+  export async function removeContact(contactId) {
+    try{ 
+        const data = await fs.readFile(contactsPath); 
+        const contacts = JSON.parse(data);
+        let deletedContact = contacts.find(contact=>contact.id===contactId);
+        if(deletedContact){
+            const newContacts =  contacts.filter(contact=>contact.id!==contactId);
+            fs.writeFile(contactsPath,JSON.stringify(newContacts));
+            console.log("Contact deleted")
+            console.log(deletedContact)
+        }
+        else{ console.log("Contact doesn`t exists")}
+        }
+    catch (err) {
+           console.log('Something went wrong..');
+           console.log(err.message);
+         }
+  }
+  
+  export async function addContact(name, email, phone) {
+    try{ 
+      const data = await fs.readFile(contactsPath); 
+      const contacts = JSON.parse(data);
+    const newContact={
+        id: nanoid(),
+        name: name,
+        email: email,
+        phone: phone,
+        }
+    await fs.writeFile(contactsPath,JSON.stringify([...contacts,newContact])); 
+    console.log('Contacts updated with')
+    console.log(newContact)}
+    catch (err) {
+        console.log('Something went wrong..');
+        console.log(err.message);
+      }
+
+  }
